a355459798f940b3746327cac2d53218a5543a73
inga-divra <ingady@gmail.com>
2024-07-24 12:07:43 +0300

    created components and added content into Hero.jsx

---
+++ src/Hero.jsx
@@ -0,0 +1,28 @@
+import heroImg from './assets/hero.svg'
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="hero-center">
+                <div className="hero-title">
+                    <h1>Contenful CMS</h1>
+                    <p>
+                        This project aims to provide a comprehensive understanding of working with a headless CMS,
+                        including setting up a Contentful account and utilizing it to store,
+                        fetch, and display data in a React app. Additionally, the project ensures
+                        that any changes made to the cloud-stored data via the graphical
+                        interface are automatically rebuilt by Netlify.
+                    </p>
+                </div>
+                <div className="img-container">
+                    <img
+                        className="img"
+                        src={heroImg}
+                        alt="woman and the browser" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
