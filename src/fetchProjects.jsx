14e39ff60b70bc43e22905020442ee2582902a39
inga-divra <ingady@gmail.com>
2024-07-24 19:50:20 +0300

    Updated the Projects component to use a custom hook for fetching projects from Contentful and display a loading message until data is fetched

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,5 +1,5 @@
-import { createClient } from 'contentful';
 import { useState, useEffect } from 'react';
+import { createClient } from 'contentful';
 
 const client = createClient({
     space: 'p2yzlou2fp2m',
@@ -9,30 +9,28 @@ const client = createClient({
 
 //custom hook
 export const useFetchProjects = () => {
-    const [loading, setLoading] = useState(true)
-    const [projects, setProjects] = useState([])
+    const [loading, setLoading] = useState(true);
+    const [projects, setProjects] = useState([]);
 
     const getData = async () => {
         try {
-            const response = await client.getEntries({ content_type: 'projects' })
+            const response = await client.getEntries({ content_type: 'projects' });
             const projects = response.items.map((item) => {
-                const { title, url, image } = item.fields
-                const id = item.sys.id
-                const img = image?.fields?.file?.url
-                return { title, url, id, img }
-            })
-            setProjects(projects)
-            setLoading(false)
+                const { title, url, image } = item.fields;
+                const id = item.sys.id;
+                const img = image?.fields?.file?.url;
+                return { title, url, id, img };
+            });
+            setProjects(projects);
         } catch (error) {
             console.log(error);
+        } finally {
             setLoading(false)
         }
-    }
+    };
 
     useEffect(() => {
-        getData()
-
-    }, [])
-
-    return { loading, projects }
-}
+        getData();
+    }, []);
+    return { loading, projects };
+};
