850362ad0352e0f253a4d620404e5f522e03e04c
inga-divra <ingady@gmail.com>
2024-07-24 19:54:56 +0300

    created .env file for api key

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -4,7 +4,7 @@ import { createClient } from 'contentful';
 const client = createClient({
     space: 'p2yzlou2fp2m',
     environment: 'master',
-    accessToken: 'fnMkwsTiuaQLmkh5buW1SJH6fWBeS3H6lEeAotnyi2I',
+    accessToken: import.meta.env.VITE_API_KEY,
 });
 
 //custom hook
